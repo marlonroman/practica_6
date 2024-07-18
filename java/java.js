576235c1f8c4b91c67cf89a5c734bf3df30f9c5a
Marlon Andrés Román Ríos <marlon210499@gmail.com>
2024-07-17 22:35:47 -0500

    casi no lo logro

--- java/java.js
+++ java/java.js
@@ -1,16 +1,27 @@
 function tirarDado() {
 
     const numDado = document.getElementById("numeroDado").value;
-    const resultadoDado = document.getElementById("numeroDado");
+    const resultadoDado = document.getElementById("dadoResultado");
     const dadoImagenes = document.getElementById("dadoImagenes");
     const values = [];
-    const images = [];
 
-    for(let i = 0; i < numDado; i++){
+    dadoImagenes.innerHTML = ('');
+
+    for( i = 0; i < numDado; i++){
         const value = Math.floor(Math.random()*6)+1;
+        console.log(value);
         values.push(value);
-        images.push('<img src="images/${value}.png">');
+        const img = document.createElement('img');
+                img.src = `images/${value}.png`;
+                img.alt = `Dado ${value}`;
+                img.style.width = '200px';
+                img.style.height = '200px';
+                dadoImagenes.appendChild(img);
+
+
     }
 
+    console.log(values);
+    resultadoDado.textContent = `dado: ${values.join(',')}`;
     
 }
